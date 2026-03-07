b8d0d951c12b4c1183250c625d3a2ae8b9b2d923
Larry Mei <91803291+syndromenuts@users.noreply.github.com>
2026-03-06 16:40:40 -0800

    Pass all tests for 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(bound1, bound2) {
+    if (bound1 < 0 || bound2 < 0) {
+        return "ERROR";
+    }
 
+    if (!Number.isInteger(bound1) || !Number.isInteger(bound2)) {
+        return "ERROR";
+    }
+
+    
+
+    const left = bound1 < bound2 ? bound1 : bound2;
+    const right = bound1 > bound2 ? bound1 : bound2;
+
+    let sum = 0;
+    for (let i = left ; i <= right; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
