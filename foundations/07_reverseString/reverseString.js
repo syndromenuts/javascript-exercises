5854ee811004a9b28c20df2ff784b421d43bd9b7
Larry Mei <91803291+syndromenuts@users.noreply.github.com>
2026-03-06 16:23:26 -0800

    Pass all tests for 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    // For loop starting from last character and going to first character
+    // add character to a result string
 
+    let result = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        result += str[i];
+    }
+
+    return result;
 };
 
 // Do not edit below this line
