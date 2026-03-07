7ef1e3b596755642844c87a6e8e9583b99bd9a7b
Larry Mei <91803291+syndromenuts@users.noreply.github.com>
2026-03-06 17:02:26 -0800

    Pass all tests for 11_tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInFahrenheit) {
+  const tempInCelsius = (tempInFahrenheit - 32) * 5 / 9;
+  const rounded = Math.round(tempInCelsius * 10) / 10;
+
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInCelsius) {
+  const tempInFahrenheit = tempInCelsius * 9 / 5 + 32
+  const rounded = Math.round(tempInFahrenheit * 10) / 10;
+
+  return rounded;
 };
 
 // Do not edit below this line
