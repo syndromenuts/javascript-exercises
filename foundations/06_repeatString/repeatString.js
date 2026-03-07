7b45b957a56557f1840de1c2aedaf68de3e672f0
Larry Mei <91803291+syndromenuts@users.noreply.github.com>
2026-03-06 16:17:06 -0800

    Pass all tests for 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, numRepeats) {
+    if (numRepeats < 0) {
+        return "ERROR";
+    }
+    
+    let result = "";
+    for (let i = 0; i < numRepeats; i++) {
+        result += str;
+    }
 
+    return result;
 };
 
 // Do not edit below this line
