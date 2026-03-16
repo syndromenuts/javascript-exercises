22a08b86efd8350962dada494420d9cd3d465fe6
Larry Mei <91803291+syndromenuts@users.noreply.github.com>
2026-03-16 12:06:57 -0700

    Pass all tests for 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const ALPHANUMERICAL_REGEX = /[^\w]/g;
+    str = str
+            .replace(ALPHANUMERICAL_REGEX, "")
+            .toLowerCase();
 
+    const reversed = str.split("").reverse().join("");
+
+    return str === reversed;
 };
 
 // Do not edit below this line
