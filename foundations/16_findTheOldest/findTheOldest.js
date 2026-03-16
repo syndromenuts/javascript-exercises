2278d33d931f35854c6051a7ff7db8ffeb6be78a
Larry Mei <91803291+syndromenuts@users.noreply.github.com>
2026-03-16 12:40:30 -0700

    Pass all tests for 16_findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,27 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.reduce((runningMax, person) => {
+        let runningMaxAge;
+        if (runningMax.yearOfDeath === undefined) {
+            runningMaxAge = new Date().getFullYear() - runningMax.yearOfBirth;
+        } else {
+            runningMaxAge = runningMax.yearOfDeath - runningMax.yearOfBirth;
+        }
 
+        let personAge;
+        if (person.yearOfDeath === undefined) {
+            personAge = new Date().getFullYear() - person.yearOfBirth;
+        } else {
+            personAge = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        if (personAge > runningMaxAge) {
+            return person;
+        } else {
+            return runningMax;
+        }
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
